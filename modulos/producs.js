17d5d93e3013b65cdef936eb762bd2a5f667b1fb
darrell <jafet.dna@gmail.com>
2023-06-06 15:21:34 -0600

    .....

---
+++ modulos/producs.js
@@ -0,0 +1,23 @@
+class Product {
+    constructor(title, color, price, joke) {
+      this.title = title;
+      this.color = color;
+      this.price = price;
+      this.joke = joke;
+    }
+  
+    render() {
+      // Generar el markup del producto en el carrito
+      // Puedes utilizar template literals o manipulación del DOM para crear el HTML
+      // Por ejemplo:
+      return `<div class="cart-product">
+                <h3>${this.title} (${this.color})</h3>
+                <p>Price: ${this.price}</p>
+                <p>Joke: ${this.joke}</p>
+                <button class="remove-button">Remove</button>
+              </div>`;
+    }
+  }
+  
+  export default Product;
+  
