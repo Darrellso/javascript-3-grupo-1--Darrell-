a798a732d63003e33f092643646bc961592c913e
darrell <jafet.dna@gmail.com>
2023-06-05 23:33:57 -0600

    ...

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import { initProducts } from './components/other-products.js';
 import { initVisualizer } from './components/visualizer.js';
 import { initDetails } from './components/details.js';
 import { initColors } from './components/colors.js';
+import getRandomJoke from './components/randomJoke.js';
 
 const jokeContainer = document.querySelector("#main-joke");
+const randomJokeButton = document.querySelector("#randomJokeButton");
 
 const urlParams = new URLSearchParams(window.location.search);
 const joke = urlParams.get("joke");
@@ -11,9 +13,18 @@ const joke = urlParams.get("joke");
 if (joke) {
   jokeContainer.textContent = joke;
 }
-
-
+getRandomJoke();
 initVisualizer();
 initDetails();
 initProducts();
 initColors();
+
+randomJokeButton.addEventListener("click", async () => {
+  try {
+    const joke = await getRandomJoke();
+    jokeContainer.textContent = joke;
+  } catch (error) {
+    console.error(error);
+    jokeContainer.textContent = "No se pudo obtener el chiste";
+  }
+});
