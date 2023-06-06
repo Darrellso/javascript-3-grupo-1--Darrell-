11768368f1b4ecb399cfd7b40af4d8defca76cab
darrell <jafet.dna@gmail.com>
2023-06-06 01:01:54 -0600

    last homework of jokes
    por error se pasaron los cambios finales a la rama de tarea1

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import { initColors } from './components/colors.js';
 import getRandomJoke from './components/randomJoke.js';
 
 const jokeContainer = document.querySelector("#main-joke");
+const jokeListItems = document.querySelectorAll("#jokeList p");
 const randomJokeButton = document.querySelector("#randomJokeButton");
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -12,8 +13,13 @@ const joke = urlParams.get("joke");
 
 if (joke) {
   jokeContainer.textContent = joke;
+  jokeListItems.forEach(item => {
+    item.textContent = joke;
+  });
 }
+
 getRandomJoke();
+
 initVisualizer();
 initDetails();
 initProducts();
@@ -23,8 +29,14 @@ randomJokeButton.addEventListener("click", async () => {
   try {
     const joke = await getRandomJoke();
     jokeContainer.textContent = joke;
+    jokeListItems.forEach(item => {
+      item.textContent = joke;
+    });
   } catch (error) {
     console.error(error);
     jokeContainer.textContent = "No se pudo obtener el chiste";
+    jokeListItems.forEach(item => {
+      item.textContent = "No se pudo obtener el chiste";
+    });
   }
 });
